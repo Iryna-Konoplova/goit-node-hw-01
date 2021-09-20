e7d75c29f76c4b0743e1e1cc4f49616d776e5af6
Iryna Konoplova <a.irina773@gmail.com>
2021-09-20 21:40:24 +0300

    Получает, добавляет, удаляет контакты

---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+
+async function listContacts() {
+    const contacts = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(contacts);
+    console.table(allContacts)
+    return allContacts;
+};
+
+async function getContactById(contactId) {
+    const contacts = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(contacts);
+    const contactById = allContacts.findIndex(contact => contact.id === Number(contactId));
+    console.log(contactById);
+    if (contactById === -1) {
+        return null;
+    }
+    return allContacts[contactById]
+};
+
+async function removeContact(contactId) {
+    const contacts = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(contacts);
+        const contactById = allContacts.findIndex(contact => contact.id === Number(contactId));
+    if (contactById === -1) {
+        return null;
+    }
+    allContacts.splice(contactById, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    console.log(`Contact with id ${contactId} deleted`)
+    return true;
+};
+
+async function addContact(name, email, phone) {
+    const contacts = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(contacts);
+    const newContact = {
+        id: allContacts.length + 1,
+        name,
+        email,
+        phone,
+    };
+    allContacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    console.log(`Contact with name ${name}, email ${email}, phone ${phone} is added`)
+    return newContact
+};
+
+
+module.exports = {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact
+};
